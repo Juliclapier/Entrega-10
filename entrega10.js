56dbb3be30eb0b16073e06d6a34b3740a3216b1a
juliana <julianaclapier@gmail.com>
2021-02-18 13:15:11 -0300

    .

---
+++ entrega10.js
@@ -0,0 +1,19 @@
+
+var inputUno = document.getElementById("nombre")
+var inputDos = document.getElementById("pass")
+
+function mostrar(inputUno,inputDos) {
+      
+
+    return (inputDos.value, inputUno.value)
+  }
+
+    function mostrarDos (){
+     $(".div-click").click(mostrarDos)
+
+       if(inputDos.value != 'clapier' && inputUno.vale !='juliana'){
+        document.getElementById("saludo").innerHTML= "Usuario incorrecto"
+      }
+    else {document.getElementById("saludo").innerHTML= "Usuario correcto"}}
+
+    
